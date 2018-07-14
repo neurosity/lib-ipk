577c759360cd7361abe4a0e392119f217e435b86
Alex Castillo <alex@castillo.io>
2018-07-14 16:43:39 -0400

    :rocket:

---
+++ README.md
@@ -0,0 +1,33 @@
+# Notion IPK
+
+Single source of truth for all inter-process communication constants.
+
+> This is a private module published on npm. Ensure the npm user has access to the neurosity npm org before installing/publishing.
+
+## Getting started
+
+``` bash
+npm install @neurosity/ipk
+```
+
+Then import the module or the json file
+
+##### ESM
+``` js
+import ipk from "@neurosity/ipk";
+```
+
+##### CJS
+``` js
+const ipk = require("@neurosity/ipk");
+```
+
+##### JSON
+
+``` js
+import ipk from "@neurosity/ipk/dist/json/index.json";
+```
+
+TODO:
+
+* Update with latest values
---
+++ src/index.ts
@@ -0,0 +1,95 @@
+export const metrics = {
+  acceleration: {
+    x: {},
+    y: {},
+    z: {}
+  },
+  awareness: {
+    creativity: {},
+    comprehension: {},
+    attention: {},
+    meditation: {}
+  },
+  brainwaves: {
+    raw: {},
+    frequency: {},
+    psd: {}
+  },
+  channelAnalysis: {
+    FC1: {},
+    FC2: {},
+    FC3: {},
+    FC4: {},
+    CP1: {},
+    CP2: {},
+    CP3: {},
+    CP4: {}
+  },
+  emotion: {
+    joy: {},
+    sadness: {},
+    anger: {},
+    surprise: {},
+    excitement: {},
+    disappointment: {},
+    fear: {},
+    love: {},
+    stress: {},
+    interest: {}
+  },
+  facialExpression: {
+    smile: {},
+    laugh: {},
+    frown: {},
+    blink: {},
+    winkLeft: {},
+    winkRight: {}
+  },
+  kinesis: {
+    rest: {},
+    artifactDetector: {},
+    leftArm: {},
+    rightArm: {},
+    leftHandPinch: {},
+    rightHandPinch: {},
+    tongue: {},
+    jumpingJacks: {},
+    leftFoot: {},
+    rightFoot: {},
+    leftThumbFinger: {},
+    leftIndexFinger: {},
+    leftMiddleFinger: {},
+    leftRingFinger: {},
+    leftPinkyFinger: {},
+    rightThumbFinger: {},
+    rightIndexFinger: {},
+    rightMiddleFinger: {},
+    rightRingFinger: {},
+    rightPinkyFinger: {},
+    mentalMath: {},
+    bitingALemon: {},
+    push: {},
+    pull: {},
+    lift: {},
+    drop: {},
+    moveLeft: {},
+    moveRight: {},
+    moveForward: {},
+    moveBackward: {},
+    rotateLeft: {},
+    rotateRight: {},
+    rotateClockwise: {},
+    rotateCounterClockwise: {},
+    disappear: {}
+  },
+  status: {
+    battery: {},
+    connected: {},
+    powered: {},
+    updating: {}
+  }
+};
+
+export default {
+  metrics
+};
